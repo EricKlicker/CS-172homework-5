ae58badd62d124ded234c40a1919342d2a3689b7
Eric Klicker <ericklicker@yahoo.com>
2016-11-01 10:11:42 -0700

    12.2

--- Hw 5 new/Hw 5 new/main.cpp
+++ Hw 5 new/Hw 5 new/main.cpp
@@ -7,12 +7,35 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+template <typename T>
+int linearSearch(const T list[], T key, int arraysize)
+{
+    for(int i=0; i < arraysize; i++)
+        if(key == list[i])
+            return i;
+    return -1;
+}
+
+
+
 
 int main()
 {
+ //12.2
+    int intList[] = { 1, 2, 4, 5, 6, 9};
+    double doubleList[] = {1.1, 5.4, 6.0, 7.8, 9.0, 3.3};
+    string stringList[] = {"Hi", "Hello", "Hey", "Greetings", "Bonjour", "Hola"};
     
-    cout << "is this thing even working...?" << endl;
+    cout << "The index of the element is " << linearSearch(intList, 5, 6) << endl;
+    cout << "The index of the element is " << linearSearch(doubleList, 1.1, 6) << endl;
+    cout << "The index of the element is " << linearSearch(stringList, string("Hello"), 6) << endl;
+    cout << endl;
+    
     
     return 0;
 }
