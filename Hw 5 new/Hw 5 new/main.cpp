4708dc4ca1ee903bf0aa727bc89bafb08d9eab24
Eric Klicker <ericklicker@yahoo.com>
2016-11-01 09:47:02 -0700

    just trying to get all of this code working

---
+++ Hw 5 new/Hw 5 new/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hw 5 new
+//
+//  Created by Eric Klicker on 11/1/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "is this thing even working...?" << endl;
+    
+    return 0;
+}
